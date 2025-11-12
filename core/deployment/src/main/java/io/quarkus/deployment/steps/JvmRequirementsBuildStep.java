3753bdf25ef4fb843081e48d11fd3a1bc7b7abce
Sanne Grinovero <sanne@hibernate.org>
2025-11-12 16:42:26 +0000

    Map static extension metadata into build items

--- core/deployment/src/main/java/io/quarkus/deployment/steps/JvmRequirementsBuildStep.java
+++ core/deployment/src/main/java/io/quarkus/deployment/steps/JvmRequirementsBuildStep.java
@@ -1,11 +1,18 @@
 package io.quarkus.deployment.steps;
 
+import java.util.Collection;
 import java.util.List;
 
+import io.quarkus.bootstrap.model.ApplicationModel;
+import io.quarkus.bootstrap.model.ExtensionDevModeConfig;
+import io.quarkus.bootstrap.model.JvmOption;
 import io.quarkus.builder.BuildException;
 import io.quarkus.deployment.ResolvedJVMRequirements;
+import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.CapabilityBuildItem;
 import io.quarkus.deployment.builditem.ModuleOpenBuildItem;
+import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
 
 /**
  * Build step that resolves and aggregates JVM requirements for the Quarkus application.
@@ -29,6 +36,20 @@ public class JvmRequirementsBuildStep {
     @BuildStep
     ResolvedJVMRequirements resolveJVMRequirements(final List<ModuleOpenBuildItem> addOpens) throws BuildException {
         return new ResolvedJVMRequirements(addOpens);
+    }
+
+    @BuildStep
+    void remapModuleConfigurationFromExtensionMetadata(CurateOutcomeBuildItem curateOutcomeBuildItem,
+            BuildProducer<CapabilityBuildItem> producer,
+            BuildProducer<ModuleOpenBuildItem> moduleOpenProducer) {
+        final ApplicationModel appModel = curateOutcomeBuildItem.getApplicationModel();
+        for (ExtensionDevModeConfig devModeConfig : appModel.getExtensionDevModeConfig()) {
+            for (JvmOption jvmOption : devModeConfig.getJvmOptions()) {
+                String optionName = jvmOption.getName();
+                Collection<String> optionValues = jvmOption.getValues();
+            }
+        }
+
     }
 
 }
